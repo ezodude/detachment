bff1ae8731aefc7d1528e67ea6aa2881e766a10f
Ezo Saleh <ezo@the-experimenters.com>
2016-03-30 12:17:31 -0400

    Drafted interface.

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-// dtch -k some-key -s some-secret -d '2016-03-01' -f 'Daily_Report*' -o 2016-03-30'user@email-address.com'
+// dtch -k some-key -s some-secret -d '2016-03-01' -f 'Daily_Report*' -o 'attachments_2016-03-30' 'user@email-address.com'
 
 const program     = require('commander')
     , detachment = require('../');
@@ -12,10 +12,18 @@ program
 .arguments('<mailbox>')
 .option('-k, --key <context.io key>', 'Your context.io oauth consumer key.')
 .option('-s, --secret <context.io secret>', 'Your context.io oauth consumer secret.')
-.option('-d, --startdate', 'Startdate for downloading attachments.')
+.option('-d, --date-after <ISO timestamp>', 'Only include files attached to messages sent after a given timestamp.')
 .option('-o, --out <directory-path>', 'Optional output directory (defaults to \'dump\').')
-.option('-f, --filename', 'Optional attachment filename. Can include shell wildcards such as *, ? and [].')
+.option('-f, --filename <attachment-filename>', 'Optional, can include shell wildcards such as *, ? and [].')
 .action(function(mailbox) {
-  console.log('Hello mailbox');
+  const opts = {
+    key: program.key,
+    secret: program.secret,
+    version: program.version || '2.0'
+  };
+
+  const det = detachment(mailbox, opts);
+  const pullOpts = { dateAfter: program.dateAfter, outputDirectory: program.out,  filename: program.filename };
+  const progressStream = det.pull(pullOpts, (err, data) => console.log(data));
 })
 .parse(process.argv);
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 
-module.exports = {};
+const _           = require('lodash')
+    ,  h          = require('highland');
+
+module.exports = Detachment;
+
+function Detachment (mailbox, opts) {
+  if (!(this instanceof Detachment)) return new Detachment(mailbox, opts);
+  if (!opts) opts = {};
+
+  console.log('Detachment MAILBOX', mailbox);
+  console.log('Detachment OPTS', opts);
+}
+
+Detachment.prototype.pull = function (opts, cb) {
+  console.log('Detachment#pull OPTS', opts);
+  cb(null, ['attachment-a', 'attachment-b']);
+  return h();
+};
