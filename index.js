cd66d0e6afe2e4d40fe7ef96263299d271259126
Ezo Saleh <ezo@the-experimenters.com>
2016-03-31 21:55:00 -0400

    Factored out api wrapper.

--- index.js
+++ index.js
@@ -3,51 +3,16 @@
 const fs          = require('fs')
     , _           = require('lodash')
     ,  h          = require('highland')
-    , ContextIO   = require('contextio')
+    , contextio   = require('./lib/api')
     , request     = require('superagent');
 
-let ctxioClient;
-
 module.exports = Detachment;
-
-// CONTEXTIO API
-// METHOD /RESOURCE/INSTANCE_ID/SUB_RESOURCE?PARAMS
-// ctxioClient.RESOURCE(INSTANCE_ID).SUB_RESOURCE().METHOD(PARAMS).then(success_handler)
-
-const timestamp = (isoDate) => Math.floor(Date.parse(isoDate) / 1000);
-
-const getAccountId = (mailbox, cb) => {
-  // /2.0/accounts?email=someemail%40email.com&limit=1
-  ctxioClient.accounts().get({ email: mailbox, limit: 1 }).then(data => cb(null, data[0].id));
-};
-
-const getAttachments = (accountId, opts, cb) => {
-  // /2.0/accounts/id/files?date_after=timestamp&file_name=filename&limit=100
-  ctxioClient.accounts(accountId)
-  .files()
-  .get({ file_name: opts.filename, date_after: opts.dateAfter && timestamp(opts.dateAfter), limit: 100 })
-  .then(data => cb(null, data));
-};
-
-const getAttachmentContent = (accountId, fileId, cb) => {
-  // /2.0/accounts/id/files?date_after=timestamp&file_name=filename&limit=100
-  ctxioClient.accounts(accountId)
-  .files(fileId)
-  .content()
-  .get({ as_link: 1 })
-  .then(data => cb(null, data));
-};
 
 function Detachment (mailbox, opts) {
   if (!(this instanceof Detachment)) return new Detachment(mailbox, opts);
   if (!opts) opts = {};
-
   this.mailbox = mailbox;
-  ctxioClient = ContextIO({
-    key: opts.key,
-    secret: opts.secret,
-    version: opts.version
-  });
+  this.contextio = contextio(opts);
 }
 
 Detachment.prototype.pull = function (opts, cb) {
@@ -73,13 +38,13 @@ Detachment.prototype.pull = function (opts, cb) {
 };
 
 Detachment.prototype._getAccountId = function (mailbox) {
-  return h.wrapCallback(getAccountId)(mailbox);
+  return h.wrapCallback(this.contextio.getAccountId).bind(this.contextio)(mailbox);
 };
 
 Detachment.prototype._getAttachments = function (accountId, opts) {
-  return h.wrapCallback(getAttachments)(accountId, opts);
+  return h.wrapCallback(this.contextio.getAttachments).bind(this.contextio)(accountId, opts);
 };
 
 Detachment.prototype._getAttachmentContent = function (accountId, fileId) {
-  return h.wrapCallback(getAttachmentContent)(accountId, fileId);
+  return h.wrapCallback(this.contextio.getAttachmentContent).bind(this.contextio)(accountId, fileId);
 };
